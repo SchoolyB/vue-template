e4d2fc894fb0d369e266710e30e4012edd6b68d1
Marshall Burns <marshallbcodes@gmail.com>
2023-02-23 10:27:20 -0600

    Initial Commit

---
+++ client/src/pages/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <div class="index-page">
+    
+<HelloWorld/>
+  Hello template</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), Pages(), Components({
+    deep: true,
+    directoryAsNamespace: true,
+    dts: './src/components.d.ts',
+  })],
+})
